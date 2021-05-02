fb2f784c2fc349da5f65f6d413cd71acc5bccf0a
Gaurav Suhag <gauravsuhag7ds@gmail.com>
2021-05-02 17:20:11 +0530

    initial commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TodoFormComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule
